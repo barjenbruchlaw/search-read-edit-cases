c938f660bf198207641d82524f70645a27ef2041
barjenbruchlaw <brian@kcpropertylaw.com>
2022-03-28 21:00:05 -0500

    More case info

--- src/components/CaseModalTab1.js
+++ src/components/CaseModalTab1.js
@@ -3,30 +3,74 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 
 const CaseModalTab1 = () => {
 
+    const [showRentArea, setShowRentArea] = useState(true)
 
-    const [showRentArea, setShowRentArea] = useState(false)
-
-    {/*    
     const [showUDArea, setShowUDArea] = useState(false)
-    const handleUDShow = setShowUDArea(true)
-    const handleUDHide = setShowUDArea(false)
 
     const [showEVArea, setShowEVArea] = useState(false)
-    const handleEVShow = setShowEVArea(true)
-    const handleEVHide = setShowEVArea(false)
-*/}
+
 
 
     return (
-        <>
+        <div className='mt-3'>
+        Is rent delinquent?
             <Form.Check
-                type='switch'
-                onChange={e => setShowRentArea(e.target.value)}
-                label='Is rent delinquent?' />
-
+                className='ms-3'
+                inline
+                type='radio'
+                name='isRentDelinquent'
+                checked='true'
+                onChange={e => setShowRentArea(true)}
+                label='Yes' />
+            <Form.Check
+                inline
+                type='radio'
+                name='isRentDelinquent'
+                onChange={e => setShowRentArea(false)}
+                label='No' />
+            
             {showRentArea ? <RentArea /> : null}
 
-        </>
+        <br />
+        Is there a lease violation?
+            <Form.Check
+                className='ms-3'
+                inline
+                type='radio'
+                name='isLeaseViolation'
+                onChange={e => setShowUDArea(true)}
+                label='Yes' />
+            <Form.Check
+                inline
+                type='radio'
+                name='isLeaseViolation'
+                checked='true'
+                onChange={e => setShowUDArea(false)}
+                label='No' />
+            
+            {showUDArea ? <UDArea /> : null}
+
+        <br />
+        Is this an emergency eviction?
+            <Form.Check
+                className='ms-3'
+                inline
+                type='radio'
+                name='isEmergencyEviction'
+                onChange={e => setShowEVArea(true)}
+                label='Yes' />
+            <Form.Check
+                inline
+                type='radio'
+                name='isEmergencyEviction'
+                checked='true'
+                onChange={e => setShowEVArea(false)}
+                label='No' />
+            
+            {showEVArea ? <EVArea /> : null}
+
+        <br />
+        </div>
 
     )
 }
@@ -37,4 +81,15 @@ const RentArea = () => {
     )
 }
 
+const UDArea = () => {
+    return (
+        <h1>UD SELECTED</h1>
+    )
+}
+
+const EVArea = () => {
+    return (
+        <h1>EMERGENCY SELECTED</h1>
+    )
+}
 export default CaseModalTab1
